c59c978f0524665c42dd375977d1876cc5d12aee
Colins <77582749+ColinUnity@users.noreply.github.com>
2021-08-10 21:34:53 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#mad libs game, link to tutorial https://www.youtube.com/watch?v=rfscVS0vtbw (58:27)
+
+colour = input("Colour: ") #asks player what they want for a spot
+plural_noun = input("Plural Noun: ")  #asks player what they want for a spot
+celebrity = input("Celebrity: ") #asks player what they want for a spot
+
+print("Roses are " + colour) #prints string + the user input
+print(plural_noun + " are blue") #prints string + the user input
+print("I love " + celebrity) #prints string + the user input
